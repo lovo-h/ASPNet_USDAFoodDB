1a97663139783df15ec37b8bc8519ce21e8ac6a9
Hector Lovo <lovohh@gmail.com>
2018-05-17 10:30:08 -0400

    Step 1. Initialize the App

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace USDAFoodDB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
