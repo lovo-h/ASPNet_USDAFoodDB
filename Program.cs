fa937f8f938026f624479d0787f60520fdf21a68
Hector Lovo <lovohh@gmail.com>
2018-05-17 10:53:38 -0400

    Step 3. Connect to the MSSQL-Server Docker

--- Program.cs
+++ Program.cs
@@ -1,12 +1,23 @@
 ﻿using System;
+using System.Data.SqlClient;
 
-namespace USDAFoodDB
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+
+namespace USDAFoodDB {
+    class Program {
+        private const string ConnString = "Server=localhost,1433;Database=master;" +
+                                          "User Id=sa;Password=password123!;" +
+                                          "MultipleActiveResultSets=true";
+
+        static void Main() {
+            var connection = new SqlConnection(ConnString);
+
+            try {
+                connection.Open();
+                Console.WriteLine("Successfully connected.");
+                connection.Close();
+            } catch (Exception ex) {
+                Console.WriteLine("Cannot open a connection:" + ex.Message);
+            }
         }
     }
 }
